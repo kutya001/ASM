3beca12c65dbad4411cb71c55bfab319568e25d0
Kutman <kutmanomuraliev012@gmail.com>
2026-06-28 12:26:59 +0600

    build: dynamically set base path to '/' on Vercel deployments

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: '/ASM/',
+  base: process.env.VERCEL ? '/' : '/ASM/',
   plugins: [
     vue(),
     tailwindcss()
